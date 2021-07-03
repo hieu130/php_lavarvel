abb0343ecf207d9898133a6112d4a20035b4c28e
mylaptop <hieuvanphan275@gmail.com>
2021-07-03 09:11:38 +0700

    up

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -1,9 +1,9 @@
 <?php
-if (! function_exists("formateDate")){
-    function formateDate($date){
-    if ($date instanceof Carbon\Carbon)
-        $date = \Carbon\Carbon::createFromDate($date);
-    return $date->format("d/m/Y");
+
+if(!function_exists("formatDate")){
+    function formatDate($date){
+        if(! $date instanceof \Carbon\Carbon)
+            $date = \Carbon\Carbon::createFromDate($date);
+        return $date->format("d/m/Y");
     }
 }
-
